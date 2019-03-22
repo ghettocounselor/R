a581ac109fd03497db920fc9120f066509c411ee
ghettocounselor <markloessi@gmail.com>
2019-03-22 05:18:25 -0600

    commit
    commit

---
+++ Section6Lecture55-56 - Project Brief & Grammar of grahics - Movie Ratings.R
@@ -0,0 +1,15 @@
+#as always the files are here: https://www.superdatascience.com/pages/rcourse
+
+#briefing here: https://sds-platform-private.s3-us-east-2.amazonaws.com/uploads/P2-Section6-The-Challenge.pdf
+
+#data here: https://sds-platform-private.s3-us-east-2.amazonaws.com/uploads/P2-Movie-Ratings.csv
+#view in browser then save as to your local machine will create a csv
+
+#import data
+moviedata <- read.csv("Section6Datafile - Movie-Ratings.csv")
+moviedata # check data out
+head(moviedata)
+
+# Grammer of Graphics - a cool hisotry of some useful stuff to know
+# https://sds-platform-private.s3-us-east-2.amazonaws.com/uploads/P2-Section6-Grammar-Of-Graphics.pdf 
+
---
+++ Section6Lecture57 - What is a factor.R
@@ -0,0 +1,44 @@
+
+
+#-------------------- DATA Layer
+#import data
+movies <- read.csv("Section6Datafile - Movie-Ratings.csv")
+movies # check data out
+head(movies)
+
+# Grammer of Graphics - a cool hisotry of some useful stuff to know
+# https://sds-platform-private.s3-us-east-2.amazonaws.com/uploads/P2-Section6-Grammar-Of-Graphics.pdf 
+
+colnames(movies) <- c("Film","Genre","CriticRating","AudienceRating","BudgetMillions","Year")
+head(movies)
+str(movies) # see structure
+# note we have some factors and R stores these things as numbers ;)
+summary(movies) # see some nice stats
+# note the Year variable is being treated as a number, which we don't want
+# lets convert Year to a Factor
+
+factor(movies$Year) # to have a look
+# so lets do the dance... what we are doing here is using the function 'factor' to transform 
+# the variable into a factor, and we are reassigning that as the same item we started with
+movies$Year <- factor(movies$Year)
+str(movies)
+# now year is a factor!
+
+
+#----------------- AESTHETIC layer
+library(ggplot2)
+# let's make some aesthetics - define the way our data maps to what you want to see, it's not what you want to see
+ggplot(data=movies, aes(x=CriticRating, y=AudienceRating)) # note this displays an empty graph
+
+# now we'll jump ahead to GEOMETRY but only so we can see ;) 
+ggplot(data=movies, aes(x=CriticRating, y=AudienceRating)) +
+  geom_point()
+
+# add some more aesthetics
+ggplot(data=movies, aes(x=CriticRating, y=AudienceRating, colour=Genre, size=Genre)) +
+  geom_point()
+# note warning: Warning message: Using size for a discrete variable is not advised.
+ggplot(data=movies, aes(x=CriticRating, y=AudienceRating, colour=Genre, size=BudgetMillions)) +
+  geom_point()
+# budget makes a much more sensible Size element
+#>>> This is the 1st request (we will improve this)
